22dea539351d4c34811b653795860539c6be2ca3
amarjit9279586367-cyber <amarjit9279586367@gmail.com>
2026-09-22 20:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -266,7 +266,7 @@ def handle_admin_panel(chat_id):
 
 def handle_callback(cb):
     data = cb.get("data", "")
-    print(f"Callback received: {data}")
+        print("CALLBACK DATA:", data)
     chat_id = cb["message"]["chat"]["id"]
     msg_id = cb["message"]["message_id"]
     cb_id = cb["id"]
